1074d0430de34be128e2f6b1b28a1f91a3682daf
Vincent Kolaczynski <vincent.kolaczynski@ck.lu>
2024-02-23 10:23:00 +0100

    Update node.js

--- node.js
+++ node.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
@@ -8,8 +9,8 @@ const upload = multer({ dest: 'uploads/' });
 app.use(express.static('public'));
 
 app.post('/upload-image', upload.single('image'), (req, res) => {
-  // Save the image to a permanent location
-  const imagePath = 'path/to/save/image/' + req.file.filename;
+  // Save the image to the 'public' folder
+  const imagePath = 'public/' + req.file.filename;
   fs.renameSync(req.file.path, imagePath);
 
   // Send a response
