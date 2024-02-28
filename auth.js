83236a26d51bbd43b20412f8524e952faf194909
Vincent Kolaczynski <vincent.kolaczynski@ck.lu>
2024-02-28 16:22:02 +0100

    dsdsdsfsdf

--- auth.js
+++ auth.js
@@ -1,47 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    displaySessionInfo();
-    displayUserInfo();
+    displayJwt();
 });
 
-function parseJwt(token) {
-    try {
-        const base64Url = token.split('.')[1];
-        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-
-        return JSON.parse(jsonPayload);
-    } catch (e) {
-        console.error("Error parsing JWT", e);
-        return {};
-    }
-}
-
-function displaySessionInfo() {
-    // Example: Retrieve and display session info (e.g., token expiry)
+function displayJwt() {
     const idToken = localStorage.getItem('idToken'); // Assuming the token is stored here
+    const jwtElement = document.getElementById('jwt');
+    
     if (idToken) {
-        const parsedToken = parseJwt(idToken);
-        document.getElementById('sessionInfo').innerHTML = 'Token Expires: ' + new Date(parsedToken.exp * 1000).toLocaleString();
+        jwtElement.textContent = idToken;
     } else {
-        document.getElementById('sessionInfo').innerHTML = 'No session information found.';
-    }
-}
-
-function displayUserInfo() {
-    const idToken = localStorage.getItem('idToken'); // Assuming the token is stored here
-    if (idToken) {
-        const parsedToken = parseJwt(idToken);
-        const userInfoElement = document.getElementById('userInfo');
-        userInfoElement.innerHTML = ''; // Clear previous content
-        Object.entries(parsedToken).forEach(([key, value]) => {
-            // Create a new div for each key-value pair and append to userInfoElement
-            const div = document.createElement('div');
-            div.textContent = `${key}: ${value}`;
-            userInfoElement.appendChild(div);
-        });
-    } else {
-        document.getElementById('userInfo').innerHTML = 'No user information found.';
+        jwtElement.textContent = 'JWT not found.';
     }
 }
