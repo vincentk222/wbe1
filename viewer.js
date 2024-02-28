a62fb24006881a566d8124cb4294f3a621358c78
Vincent Kolaczynski <vincent.kolaczynski@ck.lu>
2024-02-28 07:41:01 +0100

    test

--- viewer.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    const authToken = localStorage.getItem('authToken');
-    if (!authToken) {
-        alert('You are not authenticated. Redirecting to login page...');
-        window.location.href = 'login.html';
-    }
-};
